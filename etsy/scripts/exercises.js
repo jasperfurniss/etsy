60f069bbf2bdde5737d84ac3dfecf5fb4bd6628f
Jasper Furniss <jasperfurniss@gmail.com>
2015-01-21 16:22:50 -0500

    etsy

--- etsy/scripts/exercises.js
+++ etsy/scripts/exercises.js
@@ -2,43 +2,53 @@
 // Show me how to calculate the average price of all items. Please console.log the average.
 // The output should be "The average price is $23.63"
 
+var total = 0;
+etsyItems.map(function(item){
+  total = total += item.price;
+  average = Math.round(total/etsyItems.length *100)/100;
+
+});
+
+console.assert("The average price is $" + average );
 
 // QUESTION TWO
 // Show me how to get an array of items that cost between $14.00 and $18.00 USD
 // The output should be the three objects, which will look something like:
 //
-// "Items that cost between $14.00 USD and $18.00 USD:"
-// [
-//   {
-//      title: "1970s Coors Banquet Glass Beer Pitcher",
-//      ...
-//   },
-//   {
-//      title: "The Three Broomsticks Customizable Beer Stein Mug, Harry Potter Inspired, hogsmeade village, harry potter gift, three broomsticks mug",
-//      ...
-//   },
-//   {
-//      title: "Hand Painted Colorful Feather Glass",
-//      ...
-//   }
-// ]
+
+
+
+var filtereditems = etsyItems.filter(function(item){
+  return (item.price > 14 && item.price < 18);
+
+
+});
+
+
+console.assert("Items that cost between $14.00 USD and $18.00 USD:" + "filtereditems");
+
 
 
 // QUESTION THREE
-// Show me how find the item with a "GBP" curreny code and print its name and price. Please console.log the one you find.
+// Show me how to find the item with a "GBP" currency code and print its name and price. Please console.log the one you find.
 // The output should be "1970s Schlitz Malt Liquor Glass Beer Pitcher costs £18"
- 
+
+var particular = etsyItems.filter(function(item){
+  return (item.currency_code = "GBP");
+});
+
+console.log(particular);
 
 // QUESTION FOUR
 // Show me how to find which items are made of wood. Please console.log the ones you find.
 // The output should be:
-// 
+//
 // SALE Mid Century Siesta Ware White Mug with Anchor - Set of 3 is made of wood.
 // Bottle cap catcher personalized. Man cave gift for him- Wooden Beer pub sign - Groomsmen wedding Gift is made of wood.
 // Medium Size, Welcome To Our Firepit-Where Friends And Marshmallows Get Toasted At The Same Time-Painted Wood Sign-Custom Colors is made of wood.
 // Magnetic Wall Mount Bottle Opener Barware Set - Stainless Steel or Black - Personalized if you like! is made of wood.
 // Engraved Pocket Knife, Personalized Groomsmen Gift, Ring Bearer Gift, Graduation Gift, 4 Knives is made of wood.
- 
+
 
 // QUESTION FIVE
 // Show me how to find which items are made of eight or more materials. Please console.log the ones you find.
@@ -54,8 +64,8 @@
 // home bar
 // beer
 // bar
-// 
-// 
+//
+//
 // The Three Broomsticks Customizable Beer Stein Mug, Harry Potter  Inspired, hogsmeade village, harry potter gift, three broomsticks mug  has 13 materials:
 // glass
 // sandblast cabinet
@@ -74,4 +84,4 @@
 
 // QUESTION 6
 // Show me how to calculate how many items were made by their sellers
-// The output should be "18 were made by their sellers" 
+// The output should be "18 were made by their sellers"
